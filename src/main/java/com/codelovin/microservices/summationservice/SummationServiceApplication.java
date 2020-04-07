d367c154f46da95b5e00378c17231a6cb752996f
Arita Malik <hello@coderport.com>
2020-04-07 17:11:50 +0530

    Connected to Eureka Naming Server

--- src/main/java/com/codelovin/microservices/summationservice/SummationServiceApplication.java
+++ src/main/java/com/codelovin/microservices/summationservice/SummationServiceApplication.java
@@ -2,10 +2,12 @@ package com.codelovin.microservices.summationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class SummationServiceApplication {
 
 	public static void main(String[] args) {
